2ba0309ab408672d6605e0e3b5c6f04a78418d09
rlawksml <rlawksml@gmail.com>
2024-04-23 23:56:35 +0900

    FIX: ts 설정 및 redux store ts로 변경작업

--- src/utils/store.tsx
+++ src/utils/store.tsx
@@ -1,28 +1,35 @@
 import { createStore } from "redux";
 
 
-// no: todoList?.length,
-// title: inputValue,
-// done: false,
-// important: false,
 
-const todoList = [
-    // {
-    //     id:"",
-    //     title:"",
-    //     done:false,
-    //     important:false,
-    // }
-]
 
-const todoReducser = (state = todoList, action)=>{
+const todoList :TodoList[]
+
+interface TodoList {
+    id : number;
+    title : string;
+    done : boolean;
+    important : boolean;
+}
+
+interface Actions {
+    id? :number;
+    no? : number;
+    title? : string;
+    done? : boolean;
+    important? : boolean;
+    todo? : string
+}  
+
+
+const todoReducser = (state : TodoList[] = todoList, action:Actions)=>{
     // console.log(action)
     switch(action.type){
         case "add":
             console.log(action)
             return [{no:action.no, todo:action.todo, done:action.done, important:action.important},...state]
         case "delete":
-            return state.filter(item => item.title !== action.todo )
+            return state.filter(item => item.title: !== action.todo )
         case "done":
             state.map(item=>
                 console.log("item", item , "action",action))
